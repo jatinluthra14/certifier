07612a918e580c17e84fc477535d157284220860
Divyansh Kulshreshtha <k.divyansh268@gmail.com>
2017-12-17 23:43:58 +0530

    Updated files, better Codacy Grade (#38)
    Updated files, Codacy Grade upgrade

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,21 +1,43 @@
 function valueChanged(){
-    if($('.customCB').is(":checked")){
+        if($(".customCB").is(":checked")){
         $(".enter_custom").hide();
-    	$(".upload_form").show();    	
+         $(".upload_form").show();      
     }
     else{
         $(".enter_custom").show();
         $(".upload_form").hide();
-    }    	
+    }     
 }
-function handleFiles(files) {
-      // Check for the various File API support.
-      if (window.FileReader) {
-          // FileReader are supported.
-          getAsText(files[0]);
-      } else {
-          alert('FileReader are not supported in this browser.');
-      }
+
+function errorHandler(evt) {
+    if(evt.target.error.name === "NotReadableError") {
+      alert("Canno't read file !");
+    }
+  }
+
+function processData(csv) {
+    var studentData = csv.split(/\r?\n|\r/);
+    var tableData = '<table class="table table-bordered table-striped">';
+    for(var count = 0; count<studentData.length; count++){
+        var cellData = studentData[count].split(",");
+        tableData += '<tr>';
+        for(var cellCount=0; cellCount<cellData.length; cellCount++){
+            if(count === 0){
+                tableData += '<th>'+cellData[cellCount]+'</th>';
+            }
+            else{
+                tableData += '<td>'+cellData[cellCount]+'</td>';
+            }
+        }
+     tableData += '</tr>';
+    }
+    tableData += '</table>';
+    $('#displayResults').html(tableData);
+}
+
+function loadHandler(event) {
+      var csv = event.target.result;
+      processData(csv);
     }
 
 function getAsText(fileToRead) {
@@ -27,32 +49,19 @@ function getAsText(fileToRead) {
       reader.onerror = errorHandler;
     }
 
-function loadHandler(event) {
-      var csv = event.target.result;
-      processData(csv);
-    }
-
-function processData(csv) {
-    var student_data = csv.split(/\r?\n|\r/);
-    var table_data = '<table class="table table-bordered table-striped">';
-    for(var count = 0; count<student_data.length; count++){
-        var cell_data = student_data[count].split(",");
-        table_data += '<tr>';
-        for(var cell_count=0; cell_count<cell_data.length; cell_count++){
-            if(count === 0){
-                table_data += '<th>'+cell_data[cell_count]+'</th>';
-            }
-            else{
-                table_data += '<td>'+cell_data[cell_count]+'</td>';
-            }
-        }
-     table_data += '</tr>';
-    }
-    table_data += '</table>';
-    $('#displayResults').html(table_data);
-}
-function errorHandler(evt) {
-    if(evt.target.error.name == "NotReadableError") {
-        alert("Canno't read file !");
+function handleFiles(files){
+      // Check for the various File API support.
+      if (window.FileReader){
+          // FileReader are supported.
+          getAsText(files[0]);
+      } 
+      else{
+          alert("FileReader are not supported in this browser.");
       }
     }
+
+
+
+
+
+
